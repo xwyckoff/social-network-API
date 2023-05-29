5a849a27df9fea701620c5bb6aeac78e1e0fbb71
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-05-29 15:45:35 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+       username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+       },
+       email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: /.+\@.+\..+/ //regex that matches only email format
+       },
+       thoughts: {
+        type: ObjectId,
+        ref: 'Thought'
+       },
+       friends: {
+        type: ObjectId,
+        ref: 'User'
+       }
+    }
+)
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+module.exports = User;
