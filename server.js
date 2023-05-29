263e6b6f03fc6651240406f60495c41d681cc57e
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-05-29 15:16:01 -0400

    added config to connect server to mongodb

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const routes = require('./routes/index');
 const express = require('express');
+const db = require('./config/connection');
 const app = express();
 const PORT = 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}!`);
+    });
+});
