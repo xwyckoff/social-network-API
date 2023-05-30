370d5be8c53891e9edddc5099381418d075c9291
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-05-30 16:57:47 -0400

    added routes for creating and removing a reaction

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const { getAllThoughts, getThoughtById, createThought } = require('../../controllers/thoughtController');
+const { createReaction, removeReaction } = require('../../controllers/reactionController');
 
 router.route('/').get(getAllThoughts).post(createThought);
 router.route('/:id').get(getThoughtById);
+//there's probably a better way to do this, but it is incredibly tedious and makes it more confusing
+router.route('/:thoughtId/reactions').post(createReaction);
+router.route('/:thoughtId/reactions/reactionId').delete(removeReaction);
 
 module.exports = router;
