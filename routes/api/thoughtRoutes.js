61f969d57821e76cc841c81df814225fe2bd62ad
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-05-30 22:13:09 -0400

    fixed typo

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,6 +6,6 @@ router.route('/').get(getAllThoughts).post(createThought);
 router.route('/:id').get(getThoughtById);
 //there's probably a better way to do this, but it is incredibly tedious and makes it more confusing
 router.route('/:thoughtId/reactions').post(createReaction);
-router.route('/:thoughtId/reactions/reactionId').delete(removeReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
